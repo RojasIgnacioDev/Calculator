6fd9978693d26848848aa2d49e51aac2da901c7c
ignaciorojasdev <ignaciorojasdev@hotmail.com>
2024-01-12 18:52:48 -0300

    Agregar archivos de proyecto.

---
+++ NumberButtons.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace Calculator;
+
+public partial class MainWindow : Window {
+
+    // handle the numbers buttons
+    private void NumberOneButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void NumberTwoButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void NumberThreeButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void NumberFourButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void NumberFiveButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void NumberSixButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void NumberSevenButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void NumberEightButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void NumberNineButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void NumberZeroButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+
+    private void DotButton_Click(object sender, RoutedEventArgs e) {
+
+    }
+}
